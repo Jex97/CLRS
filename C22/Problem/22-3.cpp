6b118371d521436816c7ace03ddf32c799556b09
jinjin2017c <746806139@qq.com>
2021-12-06 15:25:36 +0800

    Update 22-3.cpp
    添加了Euler回路的加速方法

--- C22/Problem/22-3.cpp
+++ C22/Problem/22-3.cpp
@@ -7,10 +7,9 @@ using namespace std;
 * u: 当前节点;
 * path: 欧拉回路路径;
 * G: 邻接表存储的图;
-* isVisited: 该边是否已经被访问，1是0否;
 */
 void euler(int u, vector<int>& path, const vector<vector<int>>& G, vector<vector<int>>& isVisited){
-    for(int i = 0; i < G[u][i]; ++i){
+    for(int i = 0; i < G[u][i]; ++i){       // 可通过逆序遍历，每次把结尾的元素删除来减少不必要的判定；此时需去除G的const限制
         if(!isVisited[u][i]){
             isVisited[u][i] = 1;
             euler(G[u][i], path, G, isVisited);
