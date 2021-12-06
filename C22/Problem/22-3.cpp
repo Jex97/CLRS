2dcfeb1acffcf93ed2387949bd526bb0a3ac0ce5
jinjin2017c <746806139@qq.com>
2021-12-06 14:33:07 +0800

    Update 22-3.cpp
    添加了找euler回路的算法，基于DFS, 复杂度O(E)，E为边数；

--- C22/Problem/22-3.cpp
+++ C22/Problem/22-3.cpp
@@ -1,2 +1,19 @@
 // Euler tour
-#include
+// O(E)
+#include "common.h"
+
+/*
+* u: 当前节点;
+* path: 欧拉回路路径;
+* G: 邻接表存储的图;
+* isVisited: 该边是否已经被访问，1是0否;
+*/
+void euler(int u, vector<int>& path, const vector<vector<int>>& G, vector<vector<int>>& isVisited){
+    for(int i = 0; i < G[u][i]; ++i){
+        if(!isVisited[u][i]){
+            isVisited[u][i] = 1;
+            euler(G[u][i], path, G, isVisited);
+        }
+    }
+    path.push_back(u);
+}
