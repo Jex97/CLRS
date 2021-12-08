5dfe1cc313ac8b6ac7c92dbdbb2a6e60af9c67dd
jinjin2017c <746806139@qq.com>
2021-12-08 14:51:54 +0800

    Update 22-3.cpp

--- C22/Problem/22-3.cpp
+++ C22/Problem/22-3.cpp
@@ -9,12 +9,12 @@ using namespace std;
 * G: 邻接表存储的图;
 * isVisited: 该边是否已经被访问， 1是0否
 */
-void euler(int u, vector<int>& path, const vector<vector<int>>& G, vector<vector<int>>& isVisited){
-    for(int i = 0; i < G[u][i]; ++i){       // 可通过逆序遍历，每次把结尾的元素删除来减少不必要的判定；此时需去除G的const限制
-        if(!isVisited[u][i]){
-            isVisited[u][i] = 1;
-            euler(G[u][i], path, G, isVisited);
-        }
-    }
-    path.push_back(u);
+void euler(int u, stack<int>& path, const vector<vector<int>>& G, vector<vector<int>>& isVisited) {
+	for (int i = 0; i < G[u].size(); ++i) {       // 可通过逆序遍历，每次把结尾的元素删除来减少不必要的判定；此时需去除G的const限制
+		if (!isVisited[u][i]) {
+			isVisited[u][i] = 1;
+			euler(G[u][i], path, G, isVisited);
+		}
+	}
+	path.push(u);
 }
