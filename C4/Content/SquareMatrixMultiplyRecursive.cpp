d371dec35aab8047c8c4aa467d859ec9941a2a5b
Jin Xie <746806139@qq.com>
2021-12-19 22:17:39 +0800

    修复了一些BUG

--- C4/Content/SquareMatrixMultiplyRecursive.cpp
+++ C4/Content/SquareMatrixMultiplyRecursive.cpp
@@ -2,12 +2,13 @@
 
 using namespace std;
 
+typedef vector<vector<int>> Matrix;
 
 // 递归分治矩阵乘法
 // 时间复杂度：O(n^3)
 
 Matrix SquareMatrixMultiply(const Matrix& A, const Matrix& B);
-typedef vector<vector<int>> Matrix;
+
 /*
 * SquareMatrixMultiplyRecursive: 递归矩阵乘法;
 * A: 待乘矩阵;
@@ -15,5 +16,6 @@ typedef vector<vector<int>> Matrix;
 * return: Matrix C(n*n);
 */
 Matrix SquareMatrixMultiplyRecursive(const Matrix& A, const Matrix& B) {
-	
+	//Todo
+	return Matrix();
 }
